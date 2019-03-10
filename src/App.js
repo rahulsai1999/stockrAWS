27c201a6c4a90f309450c48e064b8ec65d9dce43
Rahul Sai <noneuser2183@gmail.com>
2019-03-10 23:25:09 +0530

    register works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Route , NavLink , HashRouter} from "react-router-dom";
+import Home from "./home";
+import Stuff from "./stuff";
+import Contact from "./contact";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+      <div>
+          <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/stuff">Stuff</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/stuff" component={Stuff}></Route>
+          <Route path="/contact" component={Contact}></Route>
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
